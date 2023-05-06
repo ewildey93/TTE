8c05a61f1c814cf0bac32745926556a4516ca3c2
ewildey93 <wildeye4@gmail.com>
2023-05-06 15:51:00 -0700

    Update CoVs again and therefore RegDF, coded single variable models

--- DensityRegression.R
+++ DensityRegression.R
@@ -1,8 +1,14 @@
 library(lubridate)
 library(terra)
 library(sf)
-
-
+library(raster)
+library(tibble)
+library(data.table)
+library(tidyverse)
+library(rgdal)
+library(easystats)
+library(nlme)
+library(lme4)
 setwd("C:/Users/eliwi/OneDrive/Documents/R/TTE/TTE")
 
 
@@ -11,10 +17,17 @@ Grid <- readOGR("C:/Users/eliwi/OneDrive/Documents/Salida/GeospatialLayers/Grid3
 CamLocs <- read.csv("./CamLocs.csv")
 str(CamLocs)
 CamLocs <- CamLocs[-c(38:41),]
-#Line Length
+#Line Length- length of trails in grid cell
 LineLength <- read.csv("C:/Users/eliwi/OneDrive/Documents/R/TTE/TTE/LineLength.csv")
+LineLengthGrid <- LineLength[-5,]
+LineLengthGrid <- LineLengthGrid[, c(9,11)]
+colnames(LineLengthGrid)[1] <- "LengthGrid"
 
-#lc
+LineLength150 <- read.csv("./LineLength150.csv")
+LineLength150 <- LineLength150[-5,]
+colnames(LineLength150)[2] <- "Length150"
+LineLength150 <- LineLength150[, c(4,2)]
+#lc at different distances
 lc <- raster("C:/Users/eliwi/OneDrive/Documents/R/DeerISSFTWS/CoVs/NLCD_2019_Land_Cover_L48_20210604_JbsuwO6GkIW9V4xHbi6d.tiff")
 plot(lc)
 projection(lc)
@@ -26,35 +39,50 @@ CamLocsSF<-st_as_sf(CamLocs, coords=c("Long", "Lat"), crs=CRS("+init=epsg:4326")
 CamLocsSF<-st_transform(CamLocsSF, projection(lc))
 lc100<-raster::extract(lc2, CamLocsSF, buffer=100)
 lc250 <- raster::extract(lc2, CamLocsSF, buffer=250)
-lc400 <- raster::extract(lc2, CamLocsSF, buffer=400)
+lc385 <- raster::extract(lc2, CamLocsSF, buffer=385)
 
 z <- sort(unique(raster::values(lc2)))
 
 summaryValueslc100 <- lapply(lc100,FUN = summarizeLC,LC_classes = z)
 summaryValueslc250 <- lapply(lc250,FUN = summarizeLC,LC_classes = z)
-summaryValueslc400 <- lapply(lc400,FUN = summarizeLC,LC_classes = z)
+summaryValueslc385 <- lapply(lc385,FUN = summarizeLC,LC_classes = z)
 
 listnames <- CamLocs$Camera
 
 names(summaryValueslc100) <- listnames
 names(summaryValueslc250) <- listnames
-names(summaryValueslc400) <- listnames
-lclist <- list('LC100'=summaryValueslc100,'LC250'=summaryValueslc250,'LC400'=summaryValueslc400)
+names(summaryValueslc385) <- listnames
+lclist <- list('LC100'=summaryValueslc100,'LC250'=summaryValueslc250,'LC385'=summaryValueslc385)
 
 
 lclist <- lapply(lclist, function (x) lapply(x, function (x) as.data.frame(t(x))))
+lclist <- lapply(lclist, function (x) lapply(x, function (x) cbind(x, lc= rownames(x))))
 lclist <- lapply(lclist, function (x) rbindlist(x, idcol = T))
 
 
-Plc100 <- rbindlist(summaryValueslc100)
-Plc250 <- rbindlist(summaryValueslc250)
-Plc400 <- rbindlist(summaryValueslc400)
+LCDF <- reduce(lclist, full_join, by=c(".id", "lc"))
+colnames(LCDF)[c(2,4,5)] <- c('lc100','lc250', 'lc385')
+LCDF <- LCDF[LCDF$.id != "BUSH03",]
+colnames(LCDF)[1] <- "Camera"
+ForestDF <- LCDF[LCDF$lc == 41,]
+ShrubDF <- LCDF[LCDF$lc == 52,]
+
+
+ForestDF$Camera[7:14] <- c("BUSH1","BUSH2","BUSH4","BUSH5","BUSH6","BUSH7","BUSH8","BUSH9")
+ShrubDF$Camera[7:14] <- c("BUSH1","BUSH2","BUSH4","BUSH5","BUSH6","BUSH7","BUSH8","BUSH9")
+colnames(ForestDF)[2:5] <- c("lc100.Forest","Forest", "lc250.Forest","lc385.Forest")
+colnames(ShrubDF)[2:5] <- c("lc100.Shrub","Shrub", "lc250.Shrub","lc385.Shrub")
 #convert herbaceous cover to polygon and get distance to values
 herb <- rasterToPolygons(lc2, function (x) x == 71, n=16, dissolve = T)
+writeOGR(herb, "./", "herbPoly.shp", driver="ESRI Shapefile")
+herb <- st_read(dsn="./", layer="herbPoly.shp")
 crs(herb)
-CamLocs$distherb <- distance(CamLocsSF, herb, doEdge=T)
+crs(CamLocsSF)
+CamLocs$distidx <- st_nearest_feature(CamLocsSF, herb)
+CamLocs$distherb <- st_distance(CamLocsSF, herb[CamLocs$distidx,], by_element = T)
+CamLocs2 <- CamLocs[,-c(4:11,13)]
 
-
+#get relative activity of humans
 camdf <- read.csv("C:/Users/eliwi/OneDrive/Documents/R/TTE/TTE/wildlifeinsights5.5/images.csv")
 df <- camdf[camdf$genus=="Homo",]
 df <- df[,c(2,16,17,22)]
@@ -65,6 +93,54 @@ df$cam <- replace(x = df$cam,list=BUSH3, values = "BUSH4")
 df$week <- week(df$datetime)
 df <- df %>% group_by(cam, week)%>% mutate(HumanSum= sum(count))
 HumansatCam <- df%>%group_by(cam)%>%summarise(mean(HumanSum))
+colnames(HumansatCam)[1] <- "Camera"
+
+#join all dfs in prep for regression
+CamLocs2
+CamLocs2$Camera[7:15] <- c("BUSH1","BUSH2","BUSH3","BUSH4","BUSH5","BUSH6","BUSH7","BUSH8","BUSH9")
+CoVsList <- list(HumansatCam,CamLocs2,LineLengthGrid, LineLength150, ForestDF,ShrubDF)
+CoVs <- CoVsList%>% reduce(left_join, by="Camera")
+CoVs[CoVs$Camera == "BUSH3",]
+saveRDS(CoVs, "CoVs.rds")
+
+
+colnames(estN)[1] <- "Camera"
+RegDF <- left_join(CoVs, estN, by="Camera")
+colnames(RegDF)[2] <- "HumanAct"
+RegDF <- RegDF[!is.na(RegDF$N),]
+saveRDS(RegDF, "./RegDF.rds")
+#models
+colnames(RegDF)
+m1 <- glm(N ~ scale(HumanAct) ,family=Gamma(link="identity"), data=RegDF)
+check_model(m1)
+summary(m1)
+m2 <- glm(N ~ scale(distherb) ,family=Gamma(link="identity"), data=RegDF)
+check_model(m2)
+summary(m2)
+m3 <- glm(N ~ scale(LengthGrid) ,family=Gamma(link="identity"), data=RegDF)
+check_model(m3)
+summary(m3)
+m4 <- glm(N ~ scale(Length150) ,family=Gamma(link="identity"), data=RegDF)
+check_model(m4)
+summary(m4)
+m5 <- glm(N ~ scale(lc100.Forest) ,family=Gamma(link="identity"), data=RegDF)
+check_model(m5)
+summary(m5)
+m6 <- glm(N ~ scale(lc250.Forest) ,family=Gamma(link="identity"), data=RegDF)
+check_model(m6)
+summary(m6)
+m7 <- glm(N ~ scale(lc385.Forest) ,family=Gamma(link="identity"), data=RegDF)
+check_model(m7)
+summary(m2)
+m8 <- glm(N ~ scale(lc100.Shrub) ,family=Gamma(link="identity"), data=RegDF)
+check_model(m8)
+summary(m8)
+m9 <- glm(N ~ scale(lc250.Shrub) ,family=Gamma(link="identity"), data=RegDF)
+check_model(m9)
+summary(m9)
+m10 <- glm(N ~ scale(lc385.Shrub) ,family=Gamma(link="identity"), data=RegDF)
+check_model(m10)
+summary(m10)
 
 
 #functions
