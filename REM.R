56fd2b9545c144fd9634ef2de568af9fd4af5007
ewildey93 <wildeye4@gmail.com>
2023-05-16 12:10:00 -0700

    Update REM, got effort on cameras, and independent observations using camtrapR

--- REM.R
+++ REM.R
@@ -1,5 +1,7 @@
 library(remBoot)
 library(camtrapR)
+library(dplyr)
+library(data.table)
 
 setwd("C:/Users/eliwi/OneDrive/Documents/R/TTE/TTE")
 
@@ -38,4 +40,30 @@ intable <- data.frame(df,
                       check.names      = FALSE)        # to prevent ":" being converted to ".", e.g. in EXIF:Mak
 # sort records by station, species, then time
 outtable <- camtrapR:::assessTemporalIndependence(intable = intable, deltaTimeComparedTo = "lastRecord",columnOfInterest = "Species",stationCol = "Station",minDeltaTime = 60,camerasIndependent = FALSE)
+outtable2 <- camtrapR:::assessTemporalIndependence(intable = intable, deltaTimeComparedTo = "lastRecord",columnOfInterest = "Species",stationCol = "Station",minDeltaTime = 5,camerasIndependent = FALSE)
 intable <- intable[order(intable[, intable$Station], intable[, intable$Species], intable$DateTimeOriginal),]
+outtable3 <- outtable[outtable$DateTimeOriginal %between% study_dates,]
+
+#remove duplicate records of same species taken in same second at the same station (by the same camera, if relevant)
+metadata.tmp2 <- camtrapR:::removeDuplicatesOfRecords(metadata.tmp          = intable,
+                                           removeDuplicateRecords = TRUE,
+                                           camerasIndependent     = FALSE,
+                                           stationCol             = "Station",
+                                           speciesCol             = "Species",
+                                           cameraCol              = "Station",
+                                           current                = 1, 
+                                           total                  = 35,
+                                           max_nchar_station      = 30)
+
+grpDat <- split_dat(hDat)
+DTimes <- read.csv("C:/Users/eliwi/OneDrive/Documents/R/TTE/TTE/DeploymentTimes3.csv")
+DTimes$Start<- as.POSIXct(DTimes$Start,format="%m/%d/%Y %H:%M:%S", tz="America/Denver")
+DTimes$End<- as.POSIXct(DTimes$End,format="%m/%d/%Y %H:%M:%S", tz="America/Denver")
+study_dates <- as.POSIXct(c("2022-04-15 00:00:00", "2022-05-15 23:59:59"), tz = "America/Denver")
+DTimes$seff <- case_when(DTimes$Start %between% study_dates ~ DTimes$Start, 
+                         DTimes$Start < study_dates[1] ~ study_dates[1],
+                         DTimes$Start > study_dates[2] ~ study_dates[2])
+DTimes$eeff <- case_when(DTimes$End %between% study_dates ~ DTimes$End, 
+                         DTimes$End < study_dates[1] ~ study_dates[2],
+                         DTimes$End > study_dates[2] ~ study_dates[2])
+DTimes$tm <- as.numeric(difftime(DTimes$eeff, DTimes$seff, units="hours"))
